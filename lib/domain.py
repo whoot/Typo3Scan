ccfca24338d65c1ce1c1d9defb131ce6fcf28c1a
whoot <whoot@users.noreply.github.com>
2021-03-22 17:45:39 +0100

    0.7

--- lib/domain.py
+++ lib/domain.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
 # Typo3Scan - Automatic Typo3 Enumeration Tool
-# Copyright (c) 2014-2020 Jan Rude
+# Copyright (c) 2014-2021 Jan Rude
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,7 +20,8 @@
 import re
 import string
 import random
-import sqlite3
+import hashlib
+import sqlite3, os.path
 from colorama import Fore, Style
 import lib.request as request
 from pkg_resources import parse_version
@@ -159,56 +160,48 @@ class Domain:
 
     def search_typo3_version(self):
         """
-        This methos will search for version information.
-            The exact version can be found in the ChangeLog, therefore it will be requested first.
-            Less specific version information can be found in the NEWS or INSTALL file. 
-        """           
-        files = {'typo3_src/composer.json': '(?:"typo3/cms-core":|"typo3/cms-backend":)\s?"([0-9]+\.[0-9]+\.?[0-9x]?[0-9x]?)"',
-                'typo3_src/public/typo3/sysext/install/composer.json': '(?:"typo3/cms-core":|"typo3/cms-backend":)\s?"([0-9]+\.[0-9]+\.?[0-9x]?[0-9x]?)"',
-                'typo3_src/typo3/sysext/adminpanel/composer.json': '(?:"typo3/cms-core":|"typo3/cms-backend":)\s?"([0-9]+\.[0-9]+\.?[0-9x]?[0-9x]?)"',
-                'typo3_src/typo3/sysext/backend/composer.json': '(?:"typo3/cms-core":|"typo3/cms-backend":)\s?"(\d{1,2}\.\d{1,2}\.?[0-9]?[0-9]?)"',
-                'typo3_src/typo3/sysext/info/composer.json': '(?:"typo3/cms-core":|"typo3/cms-backend":)\s?"(\d{1,2}\.\d{1,2}\.?[0-9]?[0-9]?)"',
-                'typo3_src/ChangeLog': '[Tt][Yy][Pp][Oo]3 (\d{1,2}\.\d{1,2}\.?[0-9]?[0-9]?)',
-                'ChangeLog': '[Tt][Yy][Pp][Oo]3 (\d{1,2}\.\d{1,2}\.?[0-9]?[0-9]?)',
-                'typo3/sysext/backend/ext_emconf.php': '(?:CMS |typo3_src-)(\d{1,2}\.\d{1,2}\.?[0-9]?[0-9]?)',
-                'typo3_src/typo3/sysext/install/Start/Install.php': '(?:CMS |typo3_src-)(\d{1,2}\.\d{1,2}\.?[0-9]?[0-9]?)',
-                'typo3/sysext/install/Start/Install.php': '(?:CMS |typo3_src-)(\d{1,2}\.\d{1,2}\.?[0-9]?[0-9]?)',
-                'typo3_src/NEWS.txt': 'http://wiki.typo3.org/TYPO3_(\d{1,2}\.\d{1,2})', 
-                'typo3_src/NEWS.md': '[Tt][Yy][Pp][Oo]3 [Cc][Mm][Ss] (\d{1,2}\.\d{1,2}) - WHAT\'S NEW',
-                'NEWS.txt': 'http://wiki.typo3.org/TYPO3_(\d{1,2}\.\d{1,2})',
-                'NEWS.md': '[Tt][Yy][Pp][Oo]3 [Cc][Mm][Ss] (\d{1,2}\.\d{1,2}) - WHAT\'S NEW',
-                'typo3_src/INSTALL.md': '(?:typo3_src-)(\d{1,2}\.\d{1,2}\.?[0-9x]?[0-9]?)',
-                'typo3_src/INSTALL.txt': '(?:typo3_src-)(\d{1,2}\.\d{1,2}\.?[0-9x]?[0-9]?)',
-                'INSTALL.md': '(?:typo3_src-)(\d{1,2}\.\d{1,2}\.?[0-9x]?[0-9]?)',
-                'INSTALL.txt': '(?:typo3_src-)(\d{1,2}\.\d{1,2}\.?[0-9x]?[0-9]?)'
-                }
-
+        This method will aggressively search for version information by comparing file hashes
+        """
         version = None
-        version_path = None
-        for path, regex in files.items():
-            response = request.version_information('{}/{}'.format(self.get_path(), path), regex)
-            if response and (version is None or (len(response) > len(version))):
-                version = response
-                version_path = path
+        paths = set()
+        hash_vers = dict()
+        database = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'typo3scan.db')
+        conn = sqlite3.connect(database)
+        c = conn.cursor()
+        c.execute('SELECT * FROM core_versions')
+        data = c.fetchall()
+
+        for entry in data:
+            paths.add(entry[1])
+            hash_vers[entry[0]] = entry[2]
+
+        for path in paths:
+            response = request.get_request('{}/{}'.format(self.get_path(), path))
+            if response and response['status_code'] == 200:
+                md5_hash = hashlib.md5()
+                md5_hash.update(response['html'].encode())
+                digest = md5_hash.hexdigest()
+                if digest in hash_vers:
+                    version = hash_vers[digest]
+                    if len(version) <= 4:
+                        continue
+                    else:
+                        break
 
         print('  | \n [+] Version Information')
         if version:
+            self.set_typo3_version(version)
             print('  \u251c Identified Version: '.ljust(28) + '{}'.format(Style.BRIGHT + Fore.GREEN + version + Style.RESET_ALL))
-            print('  \u251c Version File: '.ljust(28) + '{}{}'.format(self.get_path(), version_path))
-            if len(version) == 3:
+            if len(version) <= 4:
                 print('  \u251c Could not identify exact version.')
                 react = input('  \u251c Do you want to print all vulnerabilities for branch {}? (y/n): '.format(version))
                 if react.startswith('y'):
                     version = version + '.0'
                 else:
                     return False
-            self.set_typo3_version(version)
-            # sqlite stuff
-            conn = sqlite3.connect('lib/typo3scan.db')
-            c = conn.cursor()
             c.execute('SELECT advisory, vulnerability, subcomponent, affected_version_max, affected_version_min FROM core_vulns WHERE (?<=affected_version_max AND ?>=affected_version_min)', (version, version,))
             data = c.fetchall()
-            json_list = {}
+            json_list = dict()
             if data:
                 for vulnerability in data:
                     # maybe instead use this: https://zxq9.com/archives/797
--- typo3scan.py
+++ typo3scan.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
 # Typo3Scan - Automatic Typo3 Enumeration Tool
-# Copyright (c) 2014-2020 Jan Rude
+# Copyright (c) 2014-2021 Jan Rude
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -103,7 +103,7 @@ class Typo3:
                         ext_list = extensions.search_ext_version(ext_list, args.threads)
                         json_ext = extensions.output(ext_list, database)
                     else:
-                        print ('\n  [!] No extensions found.\n')
+                        print ('\n [!] No extensions found.\n')
                     if args.json:
                         json_log = {}
                         json_log[check.get_name()] = {'Backend': check.get_backend(), 'Version': check.get_typo3_version(), 'Vulnerabilities':check.get_typo3_vulns(), 'Extensions': json_ext}
