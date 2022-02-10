d44639099e89f4514233fb0676d3fe21a936db57
whoot <whoot@users.noreply.github.com>
2022-02-10 21:14:01 +0100

    v1.0.1

--- typo3scan.py
+++ typo3scan.py
@@ -18,7 +18,7 @@
 # along with this program. If not, see [http://www.gnu.org/licenses/](http://www.gnu.org/licenses/)
 #-------------------------------------------------------------------------------
 
-__version__ = '1.0'
+__version__ = '1.0.1'
 __program__ = 'Typo3Scan'
 __description__ = 'Automatic Typo3 enumeration tool'
 __author__ = 'https://github.com/whoot'
